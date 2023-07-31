9925882ce11db114081575b9ac0794f0aaddd432
Piyush Ranjan <78551258+humhainpiyush@users.noreply.github.com>
2023-07-31 23:03:20 +0530

    Time: 2 ms (42.06%), Space: 41.1 MB (97.42%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -0,0 +1,30 @@
+class Solution {
+    public static int[] decrypt(int[] code, int k) {
+        int [] ans = new int[code.length];
+
+        if(k == 0) return ans;
+
+        if(k > 0)
+        {    
+            for(int i = 0; i < code.length; i++){
+                int sum = 0;
+                for(int j = 1; j <= k; j++){
+                    sum += code[(i+j)%code.length];
+                }
+                ans[i] = sum;
+            }
+        }
+
+        if(k < 0)
+        {
+            for(int i = 0; i < code.length; i++){
+                int sum = 0;
+                for(int j = 1; j <= -1 * k; j++){
+                    sum += code[(i - j + code.length)%code.length];
+                }
+                ans[i] = sum;
+            }
+        }
+        return ans;
+    }
+}
